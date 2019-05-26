52580e5f9f273d3bb997551c7ed6b19088b7383e
stepan mikhailyuk <stepan.mikhailyuk@team.wrike.com>
2019-05-27 02:14:38 +0300

    feat(*): autonavigate to first componentn demo if all components display

--- src/styleguide/styleguide.jsx
+++ src/styleguide/styleguide.jsx
@@ -5,6 +5,11 @@ import { isPlayground, getPlayground } from '../playground/utils';
 export default function (props) {
     let newProps = props;
 
+    if (location.hash === '') {
+        window.location.hash = `/${props.sections[0].components[0].name}`;
+        return null;
+    }
+
     if (isPlayground()) {
         newProps = {
             ...props,
