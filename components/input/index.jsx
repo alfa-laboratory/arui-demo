f5f4174857b0f250c8f91f2b262e664e859d0c44
stepancar <stepancar@hotmail.com>
2020-02-18 01:09:15 +0300

    chore(*): add withTheme to demo components

--- components/input/index.jsx
+++ components/input/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-
+import { withTheme } from 'arui-feather/cn';
 import FeatherInput from 'arui-feather/input';
 
-class Input extends React.Component {
+export class Input extends React.Component {
     render() {
         return (
             <FeatherInput view='filled' label='Инпут' />
@@ -10,4 +10,4 @@ class Input extends React.Component {
     }
 }
 
-export default Input;
+export default withTheme(Input);
